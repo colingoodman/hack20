2ac8c9fb4a0a09ecc590641e354e55ea60c4140e
John Chapple <volatileelixir@protonmail.com>
2020-11-07 09:53:30 -0600

    Client basics

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    id="app"
+    class="bg-white justify-center max-h-screen overflow-y-auto"
+  >
+    <div class="bg-gray-400 max-w-xl mx-auto rounded border border-gray-500">
+      The
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
