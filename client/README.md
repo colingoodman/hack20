471a62433934bf2da61b4dd5ee1e4079bb1ae8c7
John Chapple <volatileelixir@protonmail.com>
2020-11-07 14:30:56 -0600

    improve web page

---
+++ client/README.md
@@ -0,0 +1,4 @@
+cd client
+npm install
+npm run serve
+in browser connect to localhost:8080
--- client/src/App.vue
+++ client/src/App.vue
@@ -3,16 +3,45 @@
     id="app"
     class="bg-white justify-center max-h-screen overflow-y-auto"
   >
-    <div class="bg-gray-400 max-w-xl mx-auto rounded border border-gray-500">
-      The
+  <marquee direction="right">QUIK NEWS</marquee>
+    <div class="text-gray-800 p-1 max-w-4xl mx-auto">
+      <div class="p-2">
+        <h1 class="text-4xl">Quik News</h1>
+      </div>
+      <Cycle class="mb-16" v-for="cycle in cycles" :cycle="cycle" :key="cycle.date" />
+      <button @click="getSummaries">GET</button>
     </div>
   </div>
 </template>
 
 <script>
+import cycles from '../../file.json'
+import Cycle from './Cycle.vue'
 
 export default {
   name: 'App',
+  components: { Cycle },
+  data() {
+    return {
+      cycles: cycles
+    }
+  },
+  methods: {
+    getSummaries() {
+      const Http = new XMLHttpRequest();
+      const url='https://hackkstate2020.uc.r.appspot.com/api/v1/resources/books/all';
+      Http.open("GET", url);
+      Http.send();
+
+      Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+      }
+    },
+    getSummaries2() {
+      const url='https://hackkstate2020.uc.r.appspot.com/api/v1/resources/books/all';
+      fetch(url,{header:{'Access-Control-Allow-Origin':'*'}})
+    }
+  }
 }
 </script>
 
