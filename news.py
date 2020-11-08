ba588f11e5b35d13312b6d154e46b7a2a572fdd9
Marissa Shivers <mxs@ksu.edu>
2020-11-07 19:16:07 -0600

    Change news.py so it queries top 5 news articles from today with select keywords

--- news.py
+++ news.py
@@ -1,33 +1,29 @@
 from newsapi.newsapi_client import NewsApiClient
 import json
 import requests
+import pprint
+from datetime import datetime, timedelta, timezone
+
+timerightnow = datetime.now()
+print(timerightnow.strftime('%Y-%m-%dT%H:%M:%S'))
+
+fourhoursago = datetime.today() - timedelta(hours=24, minutes=0)
+print(fourhoursago.strftime('%Y-%m-%dT%H:%M:%S'))
 
 # Init
-newsapi = NewsApiClient(api_key='1224ab37b05c4c80a1f588ee586b15d7')
+newsapi = NewsApiClient(api_key='59cc7898a9774af8bce9ea1b0ebb6e50')
 
-all_articles = newsapi.get_everything(q='election',
-                                      sources='fox-news',
-                                      domains='foxnews.com',
-                                      from_param='2020-11-06T00:00:00',
-                                      to='2020-11-07T00:00:00',
+all_articles = newsapi.get_everything(q='election OR biden OR trump OR pandemic OR virus OR stocks OR economy OR politics OR usa OR harris OR pence OR vote',
+                                      from_param=fourhoursago,
+                                      to=timerightnow,
                                       language='en',
                                       sort_by='relevancy',
-                                      page=1)
-
-top_articles = requests.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=1224ab37b05c4c80a1f588ee586b15d7')
+                                      page_size=5)
 
 
 for thing in all_articles['articles']:
   print(json.dumps(thing, indent=4))
 
-top_articles = top_articles.json()
-
-# print to a JSON file
-# will contain top headlines
-# ordered from least recent to most recent
-with open('data.txt', 'w') as outfile:
-  print(json.dumps(top_articles['articles'], indent=4))
-
-print(type(top_articles['articles']))
-print(len(top_articles['articles']))
+print(type(all_articles['articles']))
+print(len(all_articles['articles']))
 
